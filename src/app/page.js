682131e2665ba10fcb5d9d525322c63e50d60b12
Barshon <barshonweb@gmail.com>
2024-07-11 02:44:41 +0600

    built in routes (loading,notfound & errorpage)

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@
 import Counter from "@/components/counter/counter";
 
 const Home = () => {
-  throw new Error();
   return (
     <div className="mx-2">
       <h1 className="text-red-600">hi there</h1>
