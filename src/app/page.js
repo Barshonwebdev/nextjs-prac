c6fad285a11ad18a3469ad8474048562bb605b4d
Barshon <barshonweb@gmail.com>
2024-07-11 11:09:39 +0600

    link to pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 "use client";
 
 import Counter from "@/components/counter/counter";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="mx-2">
       <h1 className="text-red-600">hi there</h1>
       <Counter/> 
+      <div className="mt-5 space-x-4">
+        <Link href={'/about'}><button className="btn btn-primary">About</button> </Link>
+        <Link href={'/jobs'}><button className="btn btn-primary">Jobs</button> </Link>
+        <Link href={'/careers'}><button className="btn btn-primary">Careers</button> </Link>
+        
+      </div>
     </div>
   );
 };
